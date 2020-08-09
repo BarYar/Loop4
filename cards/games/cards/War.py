c34bbb14e5ca72f8e7bf2bb868e365b5448810e6
BarYar <baryar102030@gmail.com>
2020-08-09 16:43:52 +0300

    GUI

--- cards/games/cards/War.py
+++ cards/games/cards/War.py
@@ -1,7 +1,7 @@
 from games.cards.CardGame import CardGame
 import random
 import time
-import tkinter
+import tkinter as tk
 import smtplib, ssl
 #פונקציה המוציא את הקלף הכי גדול
 #משחק מלחמה
@@ -11,6 +11,7 @@ port = 465  # For SSL
 # Create a secure SSL context
 context = ssl.create_default_context()
 b=False
+#שולח מייל עם הקלפים לשחקנים.
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     while not b:
         try:
@@ -22,14 +23,37 @@ with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
             b=True
     sender_email = "card.game1j@gmail@gmail.com"
     for i in range(0,4):
-        message = game.players[i].cards.__repr__().encode('utf-8')
+        message=game.players[i].cards.__repr__().encode('utf-8')
+        #message=message,f'{message},{i},{str(c)}'
+        #message = f'Hey {game.players[i].name} your cards are: {c}'.encode('utf-8')
         server.sendmail(sender_email,game.players[i].mail,message)
+        #@gmail.com
 round=1
 roundmoney=0
 cards=[]
 rcards={}
 max1=0
 max2=0
+#חלון המשחק
+window=tk.Tk()
+window.geometry("1000x1000")
+window.title("War Game")
+#window.iconbitmap(r'C:\Users\97252\PycharmProjects\Loop4\games\cards\game icons\Joker.ico')
+welcome = tk.Label(text="The War Game!", fg="green",height=8, width=15,font=(1,15))
+welcome.place(x=325,y=0)
+lframe= []
+lbutton=[]
+lplayerst=[]
+for i in range(0,1):
+    lframe.append(tk.Frame(window))
+    lplayerst.append(tk.Label(lframe[i],text=f'Name:{game.players[i].name} Money:{game.players[i].money} ',height=10,width=30,font=(1,15)))
+    lplayerst[i].place(x=325,y=i+1*175)
+    for j in range(0,5):
+        lbutton.append( tk.Button(lframe[i], text=str(j+1)))
+        lbutton[j].place(x=325,y=i+1*175)
+        lbutton[j].config(height=5, width=5)
+    lframe[i].grid(row=2, column=5)
+window.mainloop()
 for i in range(0,5):
     roundmoney=round*100*4
     for j in range(0,4):
