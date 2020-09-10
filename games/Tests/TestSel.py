434a9ec7f10ccf111af8b1f4f3e9f57c82f966c6
BarYar <68278295+BarYar@users.noreply.github.com>
2020-09-10 16:16:33 +0300

    Update TestSel.py

--- games/Tests/TestSel.py
+++ games/Tests/TestSel.py
@@ -11,15 +11,16 @@ import time
 class TestAOS(TestCase):
     #setUp
     def setUp(self):
-        path = r"C:\Users\97252\Desktop\Selenium\chromedriver.exe"
+        path = r"C:\Driver\chromedriver.exe"
         self.driver = webdriver.Chrome(path)
         self.driver.get("https://www.advantageonlineshopping.com/#/")
+        self.driver.maximize_window()
     def tearDown(self):
         self.driver.quit()
     # Ordering 2 items with different quantities and then checking if the items exist in the cart.
-    def test1(self):
-        enteredProducts= {}
-        cartProducts= {}
+    def test2(self):
+        enteredProducts= []
+        cartProducts= []
         try: #Entering the tablets category.
             element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "tabletsImg"))
@@ -27,27 +28,50 @@ class TestAOS(TestCase):
         except:
             self.fail()
         self.driver.find_element_by_id("tabletsImg").click()
-        try:#Entering the first product page.
+        # Entering the first product page.
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.ID, "16"))
+            )
+        except:
+            self.fail()
+        self.driver.find_element_by_id("16").click()
+        try:
+            element = WebDriverWait(self.driver, 10).until(
+                EC.presence_of_element_located((By.XPATH, "//h1[@class='roboto-regular ng-binding']"))
+            )
+        except:
+            self.fail()
+        #Entering the first product to the cart.
+        enteredProducts=enteredProducts+[(self.driver.find_element_by_xpath("//h1[@class='roboto-regular screen768 ng-binding']").text),3,"BLACK",
+                    self.driver.find_element_by_xpath('//h2[@class="roboto-thin screen768 ng-binding"]').text]
+        actions=ActionChains(self.driver)
+        actions.double_click(self.driver.find_element_by_xpath("//div[@class='plus']")).perform()
+        self.driver.find_element_by_name("save_to_cart").click()
+        # Entering the second product page.
+        self.driver.find_element_by_xpath("//a[@class='ng-binding']").click()
+        try:
             element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "17"))
             )
         except:
             self.fail()
-        self.driver.find_element_by_xpath("//img[@src='/catalog/fetchImage?image_id=3200']").click()
+        self.driver.find_element_by_id("17").click()
         try:
             element = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//h1[@class='roboto-regular screen768 ng-binding']"))
+                EC.presence_of_element_located((By.XPATH, "//h1[@class='roboto-regular ng-binding']"))
             )
         except:
             self.fail()
-        #Entering the first product to the cart.
-        productName=(self.driver.find_element_by_xpath("//h1[@class='roboto-regular screen768 ng-binding']").text)
-        actions=ActionChains(self.driver)
-        actions.double_click(self.driver.find_element_by_xpath("//div[@class='plus']")).perform()
-        enteredProducts[productName]=3
+        # Entering the second product to the cart.
+        enteredProducts = enteredProducts + [
+            (self.driver.find_element_by_xpath("//h1[@class='roboto-regular screen768 ng-binding']").text), 2, "BLACK",
+            self.driver.find_element_by_xpath('//h2[@class="roboto-thin screen768 ng-binding"]').text]
+        self.driver.find_element_by_xpath("//div[@class='plus']").click()
         self.driver.find_element_by_name("save_to_cart").click()
+        # Entering the third product page.
         self.driver.find_element_by_xpath("//a[@class='ng-binding']").click()
-        try:#Entering the second product page.
+        try:
             element = WebDriverWait(self.driver, 10).until(
                 EC.presence_of_element_located((By.ID, "18"))
             )
@@ -56,26 +80,36 @@ class TestAOS(TestCase):
         self.driver.find_element_by_id("18").click()
         try:
             element = WebDriverWait(self.driver, 10).until(
-                EC.presence_of_element_located((By.XPATH, "//h1[@class='roboto-regular screen768 ng-binding']"))
+                EC.presence_of_element_located((By.XPATH, "//h1[@class='roboto-regular ng-binding']"))
             )
         except:
             self.fail()
-        # Entering the second product to the cart.
-        productName = (self.driver.find_element_by_xpath("//h1[@class='roboto-regular screen768 ng-binding']").text)
+        # Entering the third product to the cart.
+        enteredProducts = enteredProducts + [
+            (self.driver.find_element_by_xpath("//h1[@class='roboto-regular screen768 ng-binding']").text), 3, "BLACK",
+            self.driver.find_element_by_xpath('//h2[@class="roboto-thin screen768 ng-binding"]').text]
         self.driver.find_element_by_xpath("//div[@class='plus']").click()
-        enteredProducts[productName] = 2
         self.driver.find_element_by_name("save_to_cart").click()
+        try:
+            element = WebDriverWait(self.driver, 30).until(
+                EC.visibility_of_element_located((By.XPATH, f'// table / tbody / tr[2] / td[2] / a / label[1]'))
+            )
+        except:
+            self.fail()
         #Adding the products in cart to the dictionary.
-        for i in range (1,3):
-            itemName=self.driver.find_element_by_xpath(f'//table/tbody/tr[{i}]//h3[@class="ng-binding"]').text
-            itemValue=self.driver.find_element_by_xpath(f'//table/tbody/tr[{i}]//label[@class="ng-binding"]').text
-            cartProducts[itemName]=int(itemValue.split()[1])
-        #Check if the items exists in the cart.
+        #The method of working with rows in table, does'nt work with that table.
+        for i in range(1, 4):
+            cartProducts.append( self.driver.find_element_by_xpath(f'//table//tr[{i}]//h3[@class="ng-binding"]').text) #Add Name
+            cartProducts.append(self.driver.find_element_by_xpath(f'//table//tr[{i}]//label[@class="ng-binding"]').text) #Add Quantity
+            cartProducts.append(self.driver.find_element_by_xpath(f'//table//tr[{i}]//span[@class="ng-binding"]').text) #Add Color
+            cartProducts.append(self.driver.find_element_by_xpath(f'//table//tr[{i}]//td[3]/p[@class="price roboto-regular ng-binding"]').text) #Add Price
+        print(enteredProducts)
+        print(cartProducts)
+        # Check if the items exists in the cart.
         for i in enteredProducts:
             self.assertTrue(i in cartProducts)
-            self.assertTrue(enteredProducts[i]==cartProducts[i])
+            self.assertTrue(enteredProducts[i] == cartProducts[i])
 
 
 if __name__=="__main__":
     unittest.main()
-
